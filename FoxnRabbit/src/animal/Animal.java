09701a92cd2da04aa9ca05d247bfb296e65c5f45
xhmm <2271102347@qq.com>
2023-01-05 17:54:43 +0800

    second commit

--- FoxnRabbit/src/animal/Animal.java
+++ FoxnRabbit/src/animal/Animal.java
@@ -9,18 +9,20 @@ public abstract class Animal {
 	private int breedableAge;
 	private int age;
 	private boolean isAlive =true;
-	
+	//构造函数
 	public Animal(int ageLimit,int breedableAge) {
 		this.ageLimit =ageLimit;
 		this.breedableAge =breedableAge;
 	}
+	//获得年龄
 	protected int getAge() {
 		return age;
 	}
-	
+	//获得寿命百分比
 	protected double getAgePercent() {
 		return (double)age /ageLimit;
 	}
+	
 	public abstract Animal breed();
 	
 	public void grow() {
@@ -34,10 +36,11 @@ public abstract class Animal {
 	public boolean isAlive() {
 		return isAlive;
 	}
-	
+	//生育能力
 	public boolean isBreedable() {
 		return (age > breedableAge);
 	}
+	//周围空白格并移动
 	public Location move(Location[] freeAdj) {
 		Location ret= null;
 		if (freeAdj.length>0 && Math.random()<0.02) {
@@ -50,11 +53,11 @@ public abstract class Animal {
 	public String toString() {
 		return " "+age+":"+(isAlive?"live":"dead");
 	}
-	
+	//被吃掉
 	public Animal feed(ArrayList<Animal> neighbour) {
 		return null;
 	}
-	
+	//增加寿命
 	public void longerlife(int addage) {
 		ageLimit+=addage;
 	}
--- hello/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author XHM
- *
- */
-module hello {
-}
